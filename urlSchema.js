6d3d7a1393598c187cd8b836b34e2c94fd8fddee
nicbenji <172269214+nicbenji@users.noreply.github.com>
2025-02-10 19:54:10 +0100

    chore: added a custom validator to the url schema via dns.lookup()

--- urlSchema.js
+++ urlSchema.js
@@ -1,12 +1,25 @@
 const mongoose = require('mongoose');
 require('dotenv');
+const dns = require('dns');
 
 mongoose.connect(process.env.MONGO_URI);
 
 const urlSchema = new mongoose.Schema({
   original_url: {
     type: String,
-    required: true
+    required: true,
+    validate: {
+      validator: async (url) => {
+        try {
+          await dns.lookup(url);
+          return true;
+        } catch (err) {
+          console.error(err);
+          return false;
+        }
+      }
+    },
+    message: 'invalid url'
   },
   short_url: {
     type: Number,
@@ -17,7 +30,7 @@ const urlSchema = new mongoose.Schema({
 urlSchema.pre('save', async function() {
   const autoIncrement = require('./autoincrement.js').autoIncrement;
   if (this.isNew) {
-    this.short_url = await autoIncrement('short_url').catch(console.error);
+    this.short_url = await autoIncrement('short_url');
   }
 });
 
@@ -27,10 +40,10 @@ const createShortUrl = async (url) => {
   const shortUrl = new Url({
     original_url: url
   });
-  const result = await shortUrl.save();
+  await shortUrl.save();
   return {
-    original_url: result.original_url,
-    short_url: result.short_url
+    original_url: shortUrl.original_url,
+    short_url: shortUrl.short_url
   };
 }
 
